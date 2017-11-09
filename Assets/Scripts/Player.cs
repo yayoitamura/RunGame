e2b01701c0afdff589b0c38b7ff2583b9705b9f9
yayoitamura <gh3520900@gmail.com>
2017-11-09 23:52:51 +0900

    Create Game Scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    private Animator animator;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //if (controller.isGrounded)
+        //{ //地面についているか判定
+            if (Input.GetButtonDown("Horizontal"))
+            {
+                Debug.Log(Input.mousePosition);
+                moveDirection.y = 5; //ジャンプするベクトルの代入
+                animator.SetTrigger("jump");
+            }
+        //}
+
+        moveDirection.y -= 10 * Time.deltaTime; //重力計算
+        controller.Move(moveDirection * Time.deltaTime); //cubeを動かす処理
+    }
+}

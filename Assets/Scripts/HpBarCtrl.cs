d30acae73d9011d2e4caacb7f09480542da26fac
yayoitamura <gh3520900@gmail.com>
2017-11-10 13:58:00 +0900

    add HpBar

---
+++ Assets/Scripts/HpBarCtrl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // ←※これを忘れずに入れる
+
+public class HpBarCtrl : MonoBehaviour
+{
+
+    Slider _slider;
+    void Start()
+    {
+        // スライダーを取得する
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+
+    float _hp = 0;
+    void Update()
+    {
+        // HP上昇
+        _hp += 0.01f;
+        if (_hp > 1)
+        {
+            // 最大を超えたら0に戻す
+            _hp = 0;
+        }
+
+        // HPゲージに値を設定
+        _slider.value = _hp;
+    }
+}

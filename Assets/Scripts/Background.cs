31e5d4e336002fd8aaf3f239c9addd21af0283a8
yayoitamura <gh3520900@gmail.com>
2017-11-10 23:50:22 +0900

    Don't move until the start button is pressed.

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -3,17 +3,26 @@
 public class Background : MonoBehaviour
 {
     // スクロールするスピード
-    public float speed = 0.1f;
+    private float speed = 0.2f;
+    private string state = "none";
 
     void Update()
     {
-        // 時間によってYの値が0から1に変化していく。1になったら0に戻り、繰り返す。
-        float x = Mathf.Repeat(Time.time * speed, 1);
+        if (state == "start")
+        {
+            // 時間によってYの値が0から1に変化していく。1になったら0に戻り、繰り返す。
+            float x = Mathf.Repeat(Time.time * speed, 1);
 
-        // Yの値がずれていくオフセットを作成
-        Vector2 offset = new Vector2(x, 0);
+            // Yの値がずれていくオフセットを作成
+            Vector2 offset = new Vector2(x, 0);
 
-        // マテリアルにオフセットを設定する
-        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+            // マテリアルにオフセットを設定する
+            GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+        }
+    }
+
+    public void StartGame()
+    {
+        state = "start";
     }
 }
